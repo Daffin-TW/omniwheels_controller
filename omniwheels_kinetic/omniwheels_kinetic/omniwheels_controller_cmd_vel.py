f91ec376b0924b4433d1eedc52989c7171e862d0
Daffin_TW <166476866+Daffin-TW@users.noreply.github.com>
2024-04-15 12:24:14 +0700

    Update omniwheels_controller_cmd_vel.py

--- omniwheels_kinetic/omniwheels_kinetic/omniwheels_controller_cmd_vel.py
+++ omniwheels_kinetic/omniwheels_kinetic/omniwheels_controller_cmd_vel.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
+from std_srvs.srv import Trigger
 from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist
 
@@ -11,12 +12,15 @@ class Omniwheels_Velocity3(Node):
 
         # Constructor
         self.cmd_vel_ = (float(), float(), float())
+        self.shooting_status_ = 0
+        self.request_ = Trigger.Request()
         
-        # Topics
+        # Topics and Client Service
         self.subcriber_ = self.create_subscription(
             Joy, 'joy', self.callback_control, 10)
         self.publisher_ = self.create_publisher(
             Twist, 'cmd_vel', 10)
+        self.client_ = self.create_client(Trigger, 'ball_shoot')
         
         # Start Info
         self.get_logger().info('Node has been started')
@@ -53,6 +57,20 @@ class Omniwheels_Velocity3(Node):
         print('---', 'cmd_vel:', msg, sep='\n')
 
 
+    # Request The Robot to Shoot
+    def request_shooting(self) -> Trigger.Response | None:
+        if self.client_.service_is_ready():
+            self.future_ = self.client_.call_async(self.request_)
+            rclpy.spin_until_future_complete(self, self.future_)
+            self.get_logger().info(
+                    f'Message: {self.response_.message}, Status: {self.response_.success}'
+                )
+            return self.future_.result()
+        else:
+            self.get_logger().info('Service is not available')
+            return None
+
+
     # Subscriber Callback
     def callback_control(self, msg: Joy) -> None:
         joystick = {
@@ -60,9 +78,18 @@ class Omniwheels_Velocity3(Node):
             'LEFTY': msg.axes[1],
             'RIGHTX': msg.axes[3],
             'LEFTSHOULDER': msg.buttons[4],
-            'RIGHTSHOULDER': msg.buttons[5]
+            'RIGHTSHOULDER': msg.buttons[5],
+            'X': msg.buttons[2]
         }
-
+        
+        # Decision to Shoot The Ball
+        if self.shooting_status_ != joystick['X']:
+            if self.shooting_status_:
+                self.shooting_status_ = 0
+            else:
+                self.shooting_status_ = 1
+                self.response_ = self.request_shooting()
+            
         # Determine The Robot Velocity Scaling
         if joystick['RIGHTSHOULDER']:
             scale = 1.0
